3687b2d3b614c3990e7ddd8eeb4ba0164d8a68ac
Igor-Kwin <sinevor13@gmail.com>
2017-06-18 16:23:17 +0300

    all functionality

---
+++ README.md
@@ -0,0 +1,11 @@
+# Angular2-ShoppingCart
+
+
+***Installation***
+
+```
+git clone
+cd project
+npm install
+npm install -g angular-cli
+No server: ng serve
---
+++ src/app/models/product-data.ts
@@ -0,0 +1,42 @@
+import { Product } from './Product';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'ASUS P-Series  ',
+    src: "/assets/img/notebook/1.jpg",
+    price: 99.99,
+    info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " },
+
+      { id: 2,
+      name: 'ASUS ZenBook UX330UA-AH54',
+      src: "/assets/img/notebook/2.jpg",
+      price: 456.99,
+      info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " },
+
+
+
+      { id: 3,
+        name: 'Acer Predator Helios',
+        src: "/assets/img/notebook/3.jpg",
+        price: 878.99,
+           info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " },
+
+      { id: 4,
+          name: 'Acer Aspire  Convertible Laptop',
+          src: "/assets/img/notebook/4.jpg",
+          price: 179.99,
+         info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " },
+
+      { id: 5,
+            name: 'HP  Laptop',
+            src: "/assets/img/notebook/5.jpg",
+            price: 669.99,
+             info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " },
+
+
+      { id: 6,
+              name: 'ASUS P-Series ',
+              src: "/assets/img/notebook/6.jpg",
+              price: 662.99,
+                 info: "Acer Aspire E 15 E5-575-33BM 15.6-Inch Full HD Notebook (Intel Core i3-7100U Processor 7th Generation , 4GB DDR4, 1TB 5400RPM Hard Drive, Intel HD Graphics 620, Windows 10 Home), Obsidian Black " }]
